628ba40e727a2a676eb9b1f2c8e41920dda04b5c
Raghavendra3103 <raghavendram3103@gmail.com>
2021-11-11 12:34:54 +0530

    user 1st commit

---
+++ framework/src/test/java/githubpPactice/Demo.java
@@ -0,0 +1,12 @@
+package githubpPactice;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void Practice()
+	{
+		System.out.println("commit to github-USER 1ST COMMIT");
+	
+	}
+}
