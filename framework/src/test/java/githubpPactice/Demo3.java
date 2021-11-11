b7e45f55e4d06014a6bee47c999f9e862ded594b
Raghavendra3103 <raghavendram3103@gmail.com>
2021-11-11 14:27:44 +0530

    user 2 commit 1

---
+++ framework/src/test/java/githubpPactice/Demo3.java
@@ -0,0 +1,12 @@
+package githubpPactice;
+
+import org.testng.annotations.Test;
+
+public class Demo3 {
+	@Test
+	public void project3()
+	{
+		System.out.println("2nd project user1 1st commit");
+	}
+
+}
