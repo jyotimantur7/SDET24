663b1dde9515ed33aa531aa121f8052ff28aeffb
Raghavendra3103 <raghavendram3103@gmail.com>
2021-11-11 14:17:20 +0530

    user1 2nd commit.

---
+++ framework/src/test/java/githubpPactice/Demo2.java
@@ -0,0 +1,15 @@
+package githubpPactice;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+
+		@Test
+		public void Practice2()
+		{
+			System.out.println("commit to github-USER 1ST COMMIT");
+			System.out.println("user 1 2nd commit");
+		
+		}
+	}
+
