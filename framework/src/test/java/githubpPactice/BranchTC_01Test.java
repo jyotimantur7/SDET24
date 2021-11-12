0274606eaffe7a237e392e7ac12aa80577840a79
Raghavendra3103 <raghavendram3103@gmail.com>
2021-11-12 10:07:35 +0530

    branch test 01

---
+++ framework/src/test/java/githubpPactice/BranchTC_01Test.java
@@ -0,0 +1,12 @@
+package githubpPactice;
+
+import org.testng.annotations.Test;
+
+public class BranchTC_01Test {
+	@Test
+	public void branchTest()
+	{
+		System.out.println("branch test 01");
+	}
+
+}
